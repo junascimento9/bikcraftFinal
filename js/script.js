b5e0d642cf361d2b2a13983d055526175090dfbb
Juliana Nascimento <91079930+junascimento9@users.noreply.github.com>
2023-04-05 01:59:13 +0000

    Final 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ATIVAR LINKS DO MENU
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -10,3 +11,60 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ATIVAR ITENS DO ORÇAMENTO
+const  parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// PERGUNTAS FREQUENTES
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", "ativa");
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// GALERIA DE IMAGENS
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens")
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) { 
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// PLUGIN
+if(window.SimpleAnime) {
+  new SimpleAnime();
+}
+
